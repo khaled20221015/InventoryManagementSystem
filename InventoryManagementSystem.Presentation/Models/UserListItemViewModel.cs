83812032f7b347135895f01f5dbe32b5284f7fab
Khaled Alsmman <khaled.omar112237@gmail.com>
2026-08-21 23:08:14 +0300

    feat(users): add view model for the users list

---
+++ InventoryManagementSystem.Presentation/Models/UserListItemViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace InventoryManagementSystem.Presentation.Models
+{
+    public class UserListItemViewModel
+    {
+        public string Id { get; set; } = string.Empty;
+
+        public string FullName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Role { get; set; } = string.Empty;
+    }
+}
