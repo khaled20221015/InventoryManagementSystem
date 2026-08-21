b2a1a0d43ea18505ffb187a69ddf57fba33887b9
Khaled Alsmman <khaled.omar112237@gmail.com>
2026-08-21 20:10:53 +0300

    feat(stock): add stock form DTO with validation rules

---
+++ InventoryManagementSystem.Business/DTOs/StockFormDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using InventoryManagementSystem.DataAccess.Models;
+
+namespace InventoryManagementSystem.Business.DTOs
+{
+    public class StockFormDto
+    {
+        [Range(1, int.MaxValue, ErrorMessage = "Please select a product.")]
+        [Display(Name = "Product")]
+        public int ProductId { get; set; }
+
+
+
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be at least 1.")]
+        [Display(Name = "Quantity")]
+        public int Quantity { get; set; } = 1;
+
+
+
+        [Required(ErrorMessage = "Transaction type is required.")]
+        [Display(Name = "Transaction type")]
+        public string TransactionType { get; set; } = TransactionTypes.In;
+    }
+}
