51ada526d94040ef0120ba16b929a8316753a04b
Khaled Alsmman <khaled.omar112237@gmail.com>
2026-08-21 22:27:27 +0300

    feat(dashboard): add chart data to the dashboard view model

--- InventoryManagementSystem.Presentation/Models/DashboardViewModel.cs
+++ InventoryManagementSystem.Presentation/Models/DashboardViewModel.cs
@@ -9,5 +9,13 @@ namespace InventoryManagementSystem.Presentation.Models
         public int TotalCategories { get; set; }
 
         public List<Product> LowStockProducts { get; set; } = new();
+
+        public List<string> CategoryNames { get; set; } = new();
+
+        public List<int> ProductsPerCategory { get; set; } = new();
+
+        public List<string> TopProductNames { get; set; } = new();
+
+        public List<int> TopProductQuantities { get; set; } = new();
     }
 }
