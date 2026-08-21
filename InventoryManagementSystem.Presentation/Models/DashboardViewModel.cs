f1252fadf9d4bdc874b419f463c754355f2e5de7
Khaled Alsmman <khaled.omar112237@gmail.com>
2026-08-21 22:02:06 +0300

    feat(ui): add dashboard view model

---
+++ InventoryManagementSystem.Presentation/Models/DashboardViewModel.cs
@@ -0,0 +1,13 @@
+﻿using InventoryManagementSystem.DataAccess.Models;
+
+namespace InventoryManagementSystem.Presentation.Models
+{
+    public class DashboardViewModel
+    {
+        public int TotalProducts { get; set; }
+
+        public int TotalCategories { get; set; }
+
+        public List<Product> LowStockProducts { get; set; } = new();
+    }
+}
