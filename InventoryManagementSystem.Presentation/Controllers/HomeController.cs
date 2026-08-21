a4dd83135c3a92843b5793f693b5a238580f19ea
Khaled Alsmman <khaled.omar112237@gmail.com>
2026-08-21 22:02:43 +0300

    feat(ui): build dashboard data and require sign in on the home page

--- InventoryManagementSystem.Presentation/Controllers/HomeController.cs
+++ InventoryManagementSystem.Presentation/Controllers/HomeController.cs
@@ -1,28 +1,44 @@
 using System.Diagnostics;
+using InventoryManagementSystem.Business.Services;
 using InventoryManagementSystem.Presentation.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InventoryManagementSystem.Presentation.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ProductService _productService;
+        private readonly CategoryService _categoryService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ProductService productService, CategoryService categoryService)
         {
-            _logger = logger;
+            _productService = productService;
+            _categoryService = categoryService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var products = await _productService.GetAllAsync();
+            var categories = await _categoryService.GetAllAsync();
+
+            var model = new DashboardViewModel
+            {
+                TotalProducts = products.Count,
+                TotalCategories = categories.Count,
+                LowStockProducts = products
+                    .Where(p => p.StockQuantity <= p.MinimumStockLevel)
+                    .ToList()
+            };
+
+            return View(model);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        [AllowAnonymous]
+        public IActionResult Privacy() => View();
 
+        [AllowAnonymous]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
